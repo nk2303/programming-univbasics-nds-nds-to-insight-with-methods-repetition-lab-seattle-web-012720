8c63e276d92cc180295adf1c410db16cb7150cbf
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-09 23:19:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -42,7 +42,7 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  
+  list_of_directors(source)
 end
 
 
