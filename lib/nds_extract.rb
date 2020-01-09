2338109700cb52b429c4d4111ff5c38d02a5c8af
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-09 23:00:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,8 +29,6 @@ def list_of_directors(source)
 end
 
 def total_gross(source)
-  # Write this implementation
-  #
   # Should use methods:
   # 1. directors_totals: returns a Hash of { dir_name => gross }
   # 2. list_of_directors: names provides an Array of directors names (use
